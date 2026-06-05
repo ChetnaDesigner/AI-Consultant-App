8b3de78fdec23a6f24579444717aa8cfbe503792
ChetnaDesigner <chetnamalhotra18@gmail.com>
2026-06-05 20:27:07 +0530

    remove files

--- src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer className="mt-10 border-t border-border bg-surface py-6 text-center text-body">
-      <p>Footer</p>
-    </footer>
-  );
-}
-
-export default Footer;
